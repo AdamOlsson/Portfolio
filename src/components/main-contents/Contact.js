d9631ebfbe22316dd64f57ac8a0d9f7cc41ee27d
Adam Olsson <adamolsson@Adams-MacBook-Pro.local>
2018-07-18 21:07:27 +0200

    added social icons

--- src/components/main-contents/Contact.js
+++ src/components/main-contents/Contact.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { SocialIcon } from 'react-social-icons';
 import '../../App.css';
+import './css/Contact.css';
 
 
 export default class Contact extends Component {
@@ -7,6 +9,10 @@ export default class Contact extends Component {
         return(
             <div className="page-header">
                 <h2>Contact Me</h2>
+                    <div className="socials-container">
+                        <SocialIcon url="mailto:olsson.95_@hotmail.com" style={{ height: 100, width: 100 }} /> 
+                        <SocialIcon url="https://www.linkedin.com/in/adam-olsson-engineer/" style={{ height: 100, width: 100 }}/>
+                    </div>
             </div>
         );
     }
